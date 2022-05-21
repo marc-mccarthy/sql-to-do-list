36d8262b0a23a6ac08e43d792735bd54376e8723
theoryofmars <marstheory20@gmail.com>
2022-05-20 20:01:44 -0500

    Setup many of the base files and folders.

---
+++ STEPS.md
@@ -0,0 +1,22 @@
+# All Steps
+
+[x] npm init --y
+[x] npm install express nodemon pg
+[x] add scripts to package.json
+[x] add the server folder
+[x] add a server.js
+[x] add a public folder
+[x] add index.html
+[x] add scripts folder
+[x] add scripts.js
+[x] add styles folder
+[x] add styles.css
+[x] add a modules folder
+[x] source CDN jquery
+[x] source CDN bootstrap
+[x] start postgresql
+[x] add postgresql database
+[x] add database.sql file
+[] add routes module
+[] add database pool module
+[] start server
---
+++ database.sql
@@ -0,0 +1,15 @@
+CREATE TABLE tasks (
+	"id" serial primary key,
+	"task" text not null,
+	"start_date" timestamp not null default current_timestamp,
+	"end_date" timestamp not null,
+	"priority" boolean not null,
+	"progress" integer not null default 0,
+	"users" varchar(10) not null default current_user
+);
+
+INSERT INTO tasks (task, end_date, priority) VALUES 
+('Mow the lawn', '05/25/22 03:25 AM', 'false'),
+('Code a To-Do-List', '05/23/22 06:10 PM', 'true'),
+('Pick up dog poo', '05/26/22 08:45 PM', 'false'),
+('Setup a tasks database', '05/24/22 10:30 AM', 'true');
