e90c5804c69e955045a071945470b258d2feca03
theoryofmars <marstheory20@gmail.com>
2022-05-23 21:29:40 -0500

    Updated database.sql file.

--- database.sql
+++ database.sql
@@ -2,15 +2,16 @@ CREATE TABLE tasks (
 	"id" serial primary key,
     "complete" boolean default 'No',
 	"task" text not null,
-	"start_date" timestamp not null default current_timestamp,
-	"end_date" timestamp not null,
+	"start_date" text not null default current_date,
+	"end_date" text not null,
 	"priority" boolean not null,
 	"progress" integer not null default 0,
-	"username" varchar(10) default current_user
+	"username" varchar(10) default current_user,
+	"finish_date" text not null default 'Incomplete'
 );
 
-INSERT INTO tasks (task, end_date, priority) VALUES 
-('Mow the lawn', '05/25/22 03:25 AM', 'false'),
-('Code a To-Do-List', '05/23/22 06:10 PM', 'true'),
-('Pick up dog poo', '05/26/22 08:45 PM', 'false'),
-('Setup a tasks database', '05/24/22 10:30 AM', 'true');
+INSERT INTO tasks (task, start_date, end_date, priority) VALUES 
+('Mow the lawn', '05/25/2022', '05/27/2022', 'false'),
+('Code a To-Do-List', '05/23/2022', '05/24/2022', 'true'),
+('Pick up dog poo', '05/26/2022', '05/27/2022', 'false'),
+('Setup a tasks database', '05/24/2022', '05/28/2022', 'true');
